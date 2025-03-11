c89bb65cab96ff57fdf7dc3cc617ce4bd396dd83
James Lumawag <64976921+Satsuu@users.noreply.github.com>
2025-03-11 17:58:20 +0800

    add scrolling motion

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,62 @@
+"use client"
+
 import HeaderSection from "@/components/header/header"
 import ProjectSection from "@/components/projects/projects"
 import AboutSection from "@/components/about/about"
 import TechnologiesSection from "@/components/technologies/technologies"
 import ContactSection from "@/components/contact/contact"
+import { motion, useScroll } from "framer-motion"
 
 export default function Home() {
+  const { scrollYProgress } = useScroll()
+
   return (
     <div>
-      <div>
+      <motion.div
+        className="fixed top-0 left-0 right-0 h-1 bg-black dark:bg-white origin-left"
+        style={{ scaleX: scrollYProgress }}
+      />
+
+      <motion.div
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.6 }}
+      >
         <HeaderSection />
-      </div>
-      <div>
+      </motion.div>
+
+      <motion.div
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.6, delay: 0.2 }}
+      >
         <ProjectSection />
-      </div>
-      <div>
+      </motion.div>
+
+      <motion.div
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.6, delay: 0.2 }}
+      >
         <TechnologiesSection />
-      </div>
-      <div>
+      </motion.div>
+
+      <motion.div
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.6, delay: 0.2 }}
+      >
         <AboutSection />
-      </div>
-      <div>
+      </motion.div>
+
+      <motion.div
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.6, delay: 0.2 }}
+      >
         <ContactSection />
-      </div>
+      </motion.div>
+
       {/* <footer className="flex flex-wrap items-center justify-center p-2 z-10">
         <p>2025 Satsu. All rights reserved.</p>
       </footer> */}
